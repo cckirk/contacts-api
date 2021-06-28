f4626eb18b488bb2c0fe1c59060a14bde6ab3c61
Christopher Kirkwood <christopherkirkwood@Christophers-MacBook-Air.local>
2021-06-28 14:14:48 -0400

    adding geocoder

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,28 +1,23 @@
 class ContactsController < ApplicationController
-  def show_the_contact
-    render json: {
-      id: contact.id,
-      first_name: contact.first_name
-      last_name: contact.last_name
-      email: contact.email
-      phone_number: contact.phone_number
-    }
-  end
-  def every_contact
-    render json: {Contact.all}
-  end
+#  def show_the_contact
+   # render json: {
+    #  id: contact.id,
+     # first_name: contact.first_name
+      #last_name: contact.last_name
+      #email: contact.email
+      #phone_number: contact.phone_number
+    #}
+  #end
+  #def every_contact
+  #  render json: {Contact.all}
+ # end
   
-  def show_latitude
-    render json: {message: "hello"}
-  end
-  
-  def show_longitude
-
-    render json: {message: "hello"}
-  end
-
-  def create_contact
+  def create
     contact = Contact.new(
+      first_name: params[:input_first_name] || contact.first_name
+      last_name: params[:input_last_name] || contact.last_name
+      email: params[:input_email] || contact.email
+      phone_number: params[:input_phone_number] || contact.phone_number
       latitude: params[:input_latitude] || contact.latitude
       longitude: params[:input_longitude] || contact.longitude
     )
